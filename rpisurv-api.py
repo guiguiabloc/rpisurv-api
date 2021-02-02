81a5047ea04d394cbadf67241e3695c4dab0b240
GuiguiAbloc <guiguiabloc@gmail.com>
2021-02-02 19:21:00 +0100

    Create rpisurv-api.py

---
+++ rpisurv-api.py
@@ -0,0 +1,65 @@
+# Gruik coded by GuiguiAbloc
+import flask
+import time
+import keyboard
+app = flask.Flask(__name__)
+
+
+@app.route('/')
+def index():
+  return 'Server Works!'
+ 
+@app.route('/camera/pause')
+def campause():
+  try:
+      keyboard.press_and_release('p')
+      return("ok") 
+  except:
+      print("Error")
+      return "Error"
+
+@app.route('/camera/resume')
+def camresume():
+  try:
+      keyboard.press_and_release('r')
+      return("ok") 
+  except:
+      print("Error")
+      return "Error"
+
+
+@app.route('/camera/1')
+def cam1():
+  try:
+      keyboard.press_and_release('F1')
+  except:
+      print("Error")
+      return "Error"
+
+@app.route('/camera/2')
+def cam2():
+  try:
+      keyboard.press_and_release('F2')
+  except:
+      print("Error")
+      return "Error"
+
+@app.route('/camera/3')
+def cam3():
+  try:
+      keyboard.press_and_release('F3')
+  except:
+      print("Error")
+      return "Error"
+      
+@app.route('/camera/4')
+def cam4():
+  try:
+      keyboard.press_and_release('F4')
+  except:
+      print("Error")
+      return "Error"
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=False)
+
